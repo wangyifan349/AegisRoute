c8a65aaa32ed4aa6f53ff8607c3f284cc8e42b0d
Alex Walker <86041125+wangyifan349@users.noreply.github.com>
2026-06-16 11:06:07 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# SPDX-License-Identifier: GPL-3.0
+# SPDX-License-Identifier: AGPL-3.0-only
 """
 server.py
 AegisRoute interactive encrypted proxy server.
